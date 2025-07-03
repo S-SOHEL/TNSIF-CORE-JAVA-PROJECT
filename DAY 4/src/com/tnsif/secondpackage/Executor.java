9cb0d1954c75ac6567b37117945f54b79e8d55e4
Shaikh Sohel <shaikhsohel6786@email.com>
2025-07-04 00:16:57 +0530

    .

--- DAY 4/src/com/tnsif/secondpackage/Executor.java
+++ DAY 4/src/com/tnsif/secondpackage/Executor.java
@@ -1,21 +1,21 @@
 package com.tnsif.secondpackage;
 
-	
 
-import firstpackage.base;
+
+import src.firstpackage.base;
 
 
 public class Executor {
 
 	public static void main(String[] args) {
-		
-		
+
+
 		//accessing different package class
-		
+
 		base b1 = new base();
-		
-		//private, default , protected and public 
-		
+
+		//private, default , protected and public
+
 		b1.methodPublic();
 		b1.varPublic=34567;
 		b1.methodPublic();
