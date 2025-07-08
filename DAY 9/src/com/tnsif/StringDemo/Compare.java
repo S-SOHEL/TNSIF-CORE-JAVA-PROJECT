7f91253aaeee7225ecb8169df7f7e8ff46bbc38d
Shaikh Sohel <shaikhsohel6786@email.com>
2025-07-09 00:23:16 +0530

    .

---
+++ DAY 9/src/com/tnsif/StringDemo/Compare.java
@@ -0,0 +1,27 @@
+package com.tnsif.StringDemo;
+
+public class Compare {
+
+	public static void main(String[] args ) {
+		
+		String s1 = ("TNSIF");
+		String s2 = ("TNSIF");
+		
+		String s3=new String ("TNSIF");
+		String s4=new String ("TNSIF");
+		
+		
+		//== equals
+		
+		
+		System.out.println("Case 1:"+ (s1==s2));
+		System.out.println("Case 2:"+ (s1==s3));
+		System.out.println("Case 3:"+ (s1==s2));
+		System.out.println("Case 4:"+ (s1==s2));
+		System.out.println("Case 1:"+ (s1==s2));
+		System.out.println("Case 1:"+ (s1==s2));
+		
+		
+		
+	}
+}
